4b741374c650cf54e4feea27b28925d4e9589292
mafeave <mari_fer_0209@hotmail.com>
2016-02-15 15:33:03 -0500

    informes finales por finalizar

---
+++ blocks/bloquesModelo/informesFinalesPorFinalizar/Sql.class.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace bloquesModelo\informesFinalesPorFinalizar;
+
+if (! isset ( $GLOBALS ["autorizado"] )) {
+	include ("../index.php");
+	exit ();
+}
+
+include_once ("core/manager/Configurador.class.php");
+include_once ("core/connection/Sql.class.php");
+
+/**
+ * IMPORTANTE: Se recomienda que no se borren registros.
+ * Utilizar mecanismos para - independiente del motor de bases de datos,
+ * poder realizar rollbacks gestionados por el aplicativo.
+ */
+class Sql extends \Sql {
+	var $miConfigurador;
+	function getCadenaSql($tipo, $variable = '') {
+		
+		/**
+		 * 1.
+		 * Revisar las variables para evitar SQL Injection
+		 */
+		$prefijo = $this->miConfigurador->getVariableConfiguracion ( "prefijo" );
+		$idSesion = $this->miConfigurador->getVariableConfiguracion ( "id_sesion" );
+		
+		switch ($tipo) {
+			
+			/**
+			 * Clausulas específicas
+			 */
+			case 'insertarRegistro' :
+				$cadenaSql = 'INSERT INTO ';
+				$cadenaSql .= $prefijo . 'pagina ';
+				$cadenaSql .= '( ';
+				$cadenaSql .= 'nombre,';
+				$cadenaSql .= 'descripcion,';
+				$cadenaSql .= 'modulo,';
+				$cadenaSql .= 'nivel,';
+				$cadenaSql .= 'parametro';
+				$cadenaSql .= ') ';
+				$cadenaSql .= 'VALUES ';
+				$cadenaSql .= '( ';
+				$cadenaSql .= '\'' . $_REQUEST ['nombrePagina'] . '\', ';
+				$cadenaSql .= '\'' . $_REQUEST ['descripcionPagina'] . '\', ';
+				$cadenaSql .= '\'' . $_REQUEST ['moduloPagina'] . '\', ';
+				$cadenaSql .= $_REQUEST ['nivelPagina'] . ', ';
+				$cadenaSql .= '\'' . $_REQUEST ['parametroPagina'] . '\'';
+				$cadenaSql .= ') ';
+				break;
+			
+			case 'actualizarRegistro' :
+				$cadenaSql = 'INSERT INTO ';
+				$cadenaSql .= $prefijo . 'pagina ';
+				$cadenaSql .= '( ';
+				$cadenaSql .= 'nombre,';
+				$cadenaSql .= 'descripcion,';
+				$cadenaSql .= 'modulo,';
+				$cadenaSql .= 'nivel,';
+				$cadenaSql .= 'parametro';
+				$cadenaSql .= ') ';
+				$cadenaSql .= 'VALUES ';
+				$cadenaSql .= '( ';
+				$cadenaSql .= '\'' . $_REQUEST ['nombrePagina'] . '\', ';
+				$cadenaSql .= '\'' . $_REQUEST ['descripcionPagina'] . '\', ';
+				$cadenaSql .= '\'' . $_REQUEST ['moduloPagina'] . '\', ';
+				$cadenaSql .= $_REQUEST ['nivelPagina'] . ', ';
+				$cadenaSql .= '\'' . $_REQUEST ['parametroPagina'] . '\'';
+				$cadenaSql .= ') ';
+				break;
+			
+			case 'buscarRegistro' :
+				
+				$cadenaSql = 'SELECT ';
+				$cadenaSql .= 'id_pagina as PAGINA, ';
+				$cadenaSql .= 'nombre as NOMBRE, ';
+				$cadenaSql .= 'descripcion as DESCRIPCION,';
+				$cadenaSql .= 'modulo as MODULO,';
+				$cadenaSql .= 'nivel as NIVEL,';
+				$cadenaSql .= 'parametro as PARAMETRO ';
+				$cadenaSql .= 'FROM ';
+				$cadenaSql .= $prefijo . 'pagina ';
+				$cadenaSql .= 'WHERE ';
+				$cadenaSql .= 'nombre=\'' . $_REQUEST ['nombrePagina'] . '\' ';
+				break;
+			
+			case 'buscarInformesPorFinalizar' :
+				
+				$cadenaSql = "SELECT * ";
+				
+				$cadenaSql .= "FROM ";
+				$cadenaSql .= "trabajosdegrado.inf_tinfo ";
+				$cadenaSql .= "WHERE ";
+				$cadenaSql .= "info_einfo='SUSTENTACION'";
+				//echo $cadenaSql;
+				break;
+			
+			case 'buscarDocente' :
+				
+				$cadenaSql = 'SELECT ';
+				$cadenaSql .= "nombre || ' ' || apellido AS Nombre ";
+				$cadenaSql .= 'FROM ';
+				$cadenaSql .= 'polux_usuario ';
+				$cadenaSql .= 'JOIN trabajosdegrado.ge_tprof ';
+				$cadenaSql .= 'ON id_usuario = prof_us ';
+				$cadenaSql .= 'WHERE ';
+				$cadenaSql .= 'prof_prof=\'' . $variable . '\' ';
+				// echo $cadenaSql;
+				break;
+			
+			case 'borrarRegistro' :
+				$cadenaSql = 'INSERT INTO ';
+				$cadenaSql .= $prefijo . 'pagina ';
+				$cadenaSql .= '( ';
+				$cadenaSql .= 'nombre,';
+				$cadenaSql .= 'descripcion,';
+				$cadenaSql .= 'modulo,';
+				$cadenaSql .= 'nivel,';
+				$cadenaSql .= 'parametro';
+				$cadenaSql .= ') ';
+				$cadenaSql .= 'VALUES ';
+				$cadenaSql .= '( ';
+				$cadenaSql .= '\'' . $_REQUEST ['nombrePagina'] . '\', ';
+				$cadenaSql .= '\'' . $_REQUEST ['descripcionPagina'] . '\', ';
+				$cadenaSql .= '\'' . $_REQUEST ['moduloPagina'] . '\', ';
+				$cadenaSql .= $_REQUEST ['nivelPagina'] . ', ';
+				$cadenaSql .= '\'' . $_REQUEST ['parametroPagina'] . '\'';
+				$cadenaSql .= ') ';
+				break;
+			
+			case 'consultarRol' :
+				$cadenaSql = 'SELECT rol_nombre ';
+				$cadenaSql .= 'FROM ';
+				$cadenaSql .= 'polux_usuario u ';
+				$cadenaSql .= 'JOIN ';
+				$cadenaSql .= 'polux_usuario_subsistema us ';
+				$cadenaSql .= 'ON u.id_usuario::varchar = us.id_usuario ';
+				$cadenaSql .= 'JOIN ';
+				$cadenaSql .= 'polux_rol r ';
+				$cadenaSql .= 'ON us.rol_id = r.rol_id ';
+				$cadenaSql .= 'WHERE ';
+				$cadenaSql .= 'u.id_usuario=\'' . $variable . '\' ';
+				break;
+		}
+		
+		return $cadenaSql;
+	}
+}
+?>
